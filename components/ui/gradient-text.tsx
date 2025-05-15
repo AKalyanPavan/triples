a1f2f33b2db357b7bd627bd95729306c48ba2845
AKalyanPavan <pavanaravelli690@gmail.com>
2025-05-16 01:05:47 +0530

    build errors

--- components/ui/gradient-text.tsx
+++ components/ui/gradient-text.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils"
 import * as React from "react"
+import { HTMLAttributes } from "react";
 
 interface GradientTextProps extends HTMLAttributes<HTMLDivElement> {
   /**
