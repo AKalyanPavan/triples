5305644b923c129e0757ab66d24494e69782a107
pranesh-centilio <104816726+pranesh-centilio@users.noreply.github.com>
2025-05-17 17:04:33 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <link rel="icon" href="/favicon.jpg" />
+      <head>
+          <script type="text/javascript" src="https://seobot.centilio.com/js/centilio_connector.js"></script>
+          <script type="text/javascript">centilio_connector_init.track(441);</script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${openSans} antialiased`}
       >
