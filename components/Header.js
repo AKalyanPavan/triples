a3a3a263961d37b10f3e79910864c2fb7cd06f5b
AKalyanPavan <pavanaravelli690@gmail.com>
2025-05-19 14:52:54 +0530

    fixing callusnow button

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,19 @@
+"use client";
+
+import React, { useEffect } from 'react';
 import Image from "next/image";
 import logo from '@/images/logo.png';
 import phone from '@/images/phone.svg';
 
 export default function Header() {
+	const onCallButtonClicked = () => {
+	    window.centilio_connector_init.submit_call()
+	}
 	return(
 		<div className="py-[20px] bg-[#ffffff] flex justify-between items-center top-[0px] sticky">
 			<Image src={logo} className="sm:w-[200px] w-[150px] sm:h-[100px] h-[75px]" alt="Triples Logo" />
 			<a href="tel:+91 7418059444" id="callUsNowButton">
-				<div className="call-us-button bg-[#E6A424] h-fit flex items-center py-[10px] px-[20px] rounded-[5px] cursor-pointer sm:text-[18px] font-medium">
+				<div className="call-us-button bg-[#E6A424] h-fit flex items-center py-[10px] px-[20px] rounded-[5px] cursor-pointer sm:text-[18px] font-medium" onClick={onCallButtonClicked}>
 					<Image src={phone} alt="Phone" />
 					<span className="text-[#ffffff] ml-[10px] font-semibold">
 						<span>Call Us</span>
